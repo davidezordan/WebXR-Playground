0bbd680fcf098cfffb9c6680ec80a84a3bfdadfe
Davide Zordan <davide.zordan2@sky.uk>
2022-11-05 18:37:36 +0000

    Adding plane detection sample

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV == 'production';
 const stylesHandler = 'style-loader';
 
 const config = {
-  entry: './src/floating-cube/index.ts',
+  entry: './src/webxr-planes/index.ts',
   output: {
     filename: 'app-[fullhash].js',
     path: path.resolve(__dirname, 'dist'),
